f88a6c97f015d381728fcfb190e8495b21345505
Ramya <ramyayandrapalli98@gmail.com>
2025-02-24 12:07:13 +0530

    modified app file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Blog</h1>
+      {/* <h1>My Blog</h1> */}
       {/* <CreatePost addPost={addPost} /> */}
       <PostList posts={posts} />
       <CreatePost addPost={addPost} />
