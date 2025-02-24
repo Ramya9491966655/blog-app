3a33832a856a2dc4f305fbe6f280ad81a1c7ac90
Ramya <ramyayandrapalli98@gmail.com>
2025-02-24 11:58:35 +0530

    imported new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import PostList from './Comp/postlist';
+// import PostList from './Comp/postlist';
 import CreatePost from './Comp/createpost';
+import PostList from './Comp/postlist';
 import './App.css';
 
 function App() {
@@ -16,8 +17,9 @@ function App() {
   return (
     <div className="App">
       <h1>My Blog</h1>
-      <CreatePost addPost={addPost} />
+      {/* <CreatePost addPost={addPost} /> */}
       <PostList posts={posts} />
+      <CreatePost addPost={addPost} />
     </div>
   );
 }
